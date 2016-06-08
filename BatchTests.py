1637cd70fc3d55664b3bc15d855df64ca3e8971c
Chris Tralie <chris.tralie@gmail.com>
2016-06-08 13:59:28 -0400

    Making progress on batch tests

--- BatchTests.py
+++ BatchTests.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 import numpy as np
 from TestPointClouds import *
 from SparseEdgeList import *
@@ -22,13 +23,27 @@ def plotDGM(dgm, color = 'b', sz = 20):
     plt.xlabel('Time of Birth')
     plt.ylabel('Time of Death')
 
+def savePD(filename, I):
+    fout = open(filename, "w")
+    for i in range(I.shape[0]):
+        fout.write("%g %g"%(X[i, 0], X[i, 1]))
+        if i < I.shape[0]-1:
+            fout.write("\n")
+    fout.close()
+
 #Wrap around Dionysus's bottleneck distance after taking the log
-def getInterleavingDist(PD1, PD2):    
-    np.savetxt("PD1.txt", np.log(PD1))
-    np.savetxt("PD2.txt", np.log(PD2))
+def getInterleavingDist(PD1, PD2):
+    savePD("PD1.txt", np.log(PD1))
+    savePD("PD2.txt", np.log(PD2))
     proc = subprocess.Popen(["./bottleneck", "PD1.txt", "PD2.txt"], stdout=subprocess.PIPE)
-    lnd = float(x.stdout.readline())
+    lnd = float(proc.stdout.readline())
     return np.exp(lnd)
+
+def getBottleneckDist(PD1, PD2):
+    savePD("PD1.txt", PD1)
+    savePD("PD2.txt", PD2)
+    proc = subprocess.Popen(["./bottleneck", "PD1.txt", "PD2.txt"], stdout=subprocess.PIPE)
+    return float(proc.stdout.readline())
 
 def parsePDs(filename):
     PDs = {}
@@ -49,7 +64,11 @@ def parsePDs(filename):
     return ret
 
 def getPDs(I, J, D, N, m):
+    if os.path.exists("temp.dimacs"):
+        os.remove("temp.dimacs")
     writeResults(I, J, D, N, "temp.dimacs")
+    if os.path.exists("temp.results"):
+        os.remove("temp.results")
     subprocess.call(["./phatclique", "-i", "temp.dimacs", "-m", "%i"%m, "-o" "temp.results"])
     return parsePDs("temp.results")
 
@@ -58,12 +77,17 @@ if __name__ == '__main__':
     X = getRandCircle(100)
     N = X.shape[0]
     (I, J, D) = makeComplex(X, 0)
-    PDs = getPDs(I, J, D, N, 4)
+    PDsFull = getPDs(I, J, D, N, 3)
+    
+    (I, J, D) = makeComplex(X, 0.2)
+    PDsApprox = getPDs(I, J, D, N, 3)
     
     plt.subplot(121)
     plt.plot(X[:, 0], X[:, 1], '.')
     plt.title('Random Point Cloud')
     plt.subplot(122)
-    plotDGM(PDs[1])
+    plotDGM(PDsFull[1], 'r', 10)
+    I = PDsApprox[1]
+    plt.plot(I[:, 0], I[:, 1], 'bx')
     plt.title('Persistence Diagram')
     plt.show()
